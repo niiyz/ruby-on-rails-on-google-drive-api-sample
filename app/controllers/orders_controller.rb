8a601f6715152d94bcdf1b3825f359081671c519
Tetsuya Yoshida <yoshida@niiyz.com>
2019-06-10 11:12:55 +0900

    画像をアップロードして権限を与えるまで。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,51 @@ require 'googleauth'
 require 'google/apis/drive_v3'
 
 class OrdersController < ApplicationController
+  before_action :auth_drive
 
   Drive = ::Google::Apis::DriveV3
 
   def index
 
-    drive = Drive::DriveService.new
+    @result = @drive_service.list_files(page_size: 10,
+                               fields: 'files(name,modified_time,web_view_link),next_page_token')
+  end
+
+  def create
+
+    file_metadata = {
+        name: 'photo.jpg'
+    }
+    file = @drive_service.create_file(file_metadata,
+                                     fields: 'id',
+                                     upload_source: 'photo.jpg',
+                                     content_type: 'image/jpeg')
+    puts "File Id: #{file.id}"
+
+    create_permission(file.id)
+
+  end
+
+  def create_permission(file_id)
+
+    user_permission = {
+        type: 'user',
+        role: 'writer',
+        email_address: 'yoshida@niiyz.com'
+    }
+    @drive_service.create_permission(file_id,
+                              user_permission,
+                              fields: 'id')
+  end
+
+  private
+
+  def auth_drive
+    @drive_service = Drive::DriveService.new
 
     auth = ::Google::Auth::ServiceAccountCredentials
                .make_creds(scope: 'https://www.googleapis.com/auth/drive')
-    drive.authorization = auth
+    @drive_service.authorization = auth
 
-    list_files = drive.list_files()
-
-    logger.debug list_files
-
-    render plain: list_files.to_s
   end
-
 end
